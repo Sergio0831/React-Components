7f10fe67cf3b2c8941fd0755de75eee892fff332
Sergio0831 <sergejs.ivcenko@gmail.com>
2023-03-11 15:30:12 +0000

    Added functionaliti on click buttons

--- voting-component/src/App.tsx
+++ voting-component/src/App.tsx
@@ -1,19 +1,66 @@
-import { useState } from 'react';
+import { MouseEventHandler, useState } from 'react';
 import VotingBtn from './components/VotingBtn';
 import { ThumbsDown, ThumbsUp } from './icons';
 import styles from './App.module.css';
 
 function App() {
+	const [likes, setLikes] = useState<number>(5);
+	const [dislikes, setDislikes] = useState<number>(1);
+
+	const [isLiked, setIsLiked] = useState<boolean>(false);
+	const [isDisliked, setIsDisliked] = useState<boolean>(false);
+
+	const handleOnClick = (e: React.MouseEvent<HTMLButtonElement>) => {
+		e.preventDefault();
+		const button: HTMLButtonElement = e.currentTarget;
+
+		if (button.name === 'like') {
+			if (isDisliked) {
+				setIsDisliked(false);
+				setDislikes((prev) => prev - 1);
+			}
+			isLiked ? setLikes((prev) => prev - 1) : setLikes((prev) => prev + 1);
+			setIsLiked(!isLiked);
+		}
+
+		if (button.name === 'dislike') {
+			if (isLiked) {
+				setIsLiked(false);
+				setLikes((prev) => prev - 1);
+			}
+			isDisliked
+				? setDislikes((prev) => prev - 1)
+				: setDislikes((prev) => prev + 1);
+			setIsDisliked(!isDisliked);
+		}
+	};
+
 	return (
-		<div className={styles.container}>
-			<h1>Was this article helpful?</h1>
-			<div className='votingBtns'>
-				<VotingBtn icon={<ThumbsUp />} />
-				<VotingBtn icon={<ThumbsDown />} />
+		<div className={styles['voting-container']}>
+			<div className={styles['voting-body']}>
+				<h1 className={styles['voting-title']}>Was this article helpful?</h1>
+				<div className={styles['voting-btns']}>
+					<VotingBtn
+						icon={<ThumbsUp />}
+						name='like'
+						handleOnClick={handleOnClick}
+						ariaLabel='like article'
+						ariaPressed={isLiked}
+					/>
+					<VotingBtn
+						icon={<ThumbsDown />}
+						name='dislike'
+						handleOnClick={handleOnClick}
+						ariaLabel='dislike article'
+						ariaPressed={isDisliked}
+					/>
+				</div>
+				<p className={styles.muted}>
+					{likes} out of {likes + dislikes} found this helpful
+				</p>
 			</div>
-			<p>5 out of 6 found this helpful</p>
-			<p>
-				Have more questions? <span>Submit a request</span>
+			<p className={styles['voting-footer']}>
+				Have more questions? <a href='#'>Submit a request</a>
 			</p>
 		</div>
 	);
--- voting-component/src/components/VotingBtn.tsx
+++ voting-component/src/components/VotingBtn.tsx
@@ -1,9 +1,32 @@
+import styles from './VotingBtn.module.css';
+
 type VotingBtnProps = {
 	icon: JSX.Element;
+	name: string;
+	ariaLabel: string;
+	ariaPressed: boolean;
+	handleOnClick: (e: React.MouseEvent<HTMLButtonElement>) => void;
 };
 
-const VotingBtn = ({ icon }: VotingBtnProps) => {
-	return <button type='button'>{icon}</button>;
+const VotingBtn = ({
+	icon,
+	name,
+	ariaLabel,
+	ariaPressed,
+	handleOnClick,
+}: VotingBtnProps) => {
+	return (
+		<button
+			type='button'
+			className={`${styles['voting-btn']} `}
+			onClick={handleOnClick}
+			aria-pressed={ariaPressed}
+			aria-label={ariaLabel}
+			name={name}
+		>
+			{icon}
+		</button>
+	);
 };
 
 export default VotingBtn;
